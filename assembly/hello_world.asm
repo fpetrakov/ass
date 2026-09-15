f5a39bf04596c68cea74e211296155ef617e05b2
fpetrakov <jakephin2015@gmail.com>
2026-09-15 10:53:26 +0300

    nasm syntax

---
+++ assembly/hello_world.asm
@@ -0,0 +1,18 @@
+section .data
+	msg db "Hello, World!", 10
+	len equ $ - msg
+
+section .text
+	default rel
+	global _start
+
+_start:
+	mov rax, 1
+	mov rdi, 1
+	lea rsi, [msg]
+	mov rdx, len
+	syscall
+
+	xor rdi, rdi 
+	mov rax, 60
+	syscall
