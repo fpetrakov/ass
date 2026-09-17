bac8a368e74be9f50cc0851e17862d073f5e2e1b
fpetrakov <jakephin2015@gmail.com>
2026-09-17 12:12:27 +0300

    dynamic response

--- asm-http-server/main.asm
+++ asm-http-server/main.asm
@@ -1,14 +1,15 @@
-%define SYS_READ 0
-%define SYS_WRITE 1
-%define SYS_CLOSE 3
-%define SYS_SOCKET 41
-%define SYS_BIND 49
-%define SYS_ACCEPT 43 
-%define SYS_LISTEN 50
-%define SYS_EXIT 60
-%define STDOUT 1
-%define EXIT_SUCCESS 0 
-%define BUF_SIZE 4112
+%define sys_read 0
+%define sys_write 1
+%define sys_open 2
+%define sys_close 3
+%define sys_socket 41
+%define sys_bind 49
+%define sys_accept 43 
+%define sys_listen 50
+%define sys_exit 60
+%define stdout 1
+%define exit_success 0 
+%define buf_size 4112
 
 section .data
 response db "HTTP/1.0 200 OK", 13, 10, 13, 10
@@ -19,11 +20,11 @@ section .text
 mov rdi, 2 ; AF_INET
 mov rsi, 1 ; SOCK_STREAM
 mov rdx, 0
-mov rax, SYS_SOCKET
+mov rax, sys_socket
 syscall
 mov r12, rax
 
-sub rsp, BUF_SIZE
+sub rsp, buf_size
 mov word [rsp], 2
 mov word [rsp+2], 0x5000
 mov dword [rsp+4], 0
@@ -32,37 +33,83 @@ mov qword [rsp+8], 0
 mov rdi, rax
 mov rsi, rsp
 mov rdx, 16
-mov rax, SYS_BIND
+mov rax, sys_bind
 syscall
 
 mov rdi, r12
 mov rsi, 0
-mov rax, SYS_LISTEN
+mov rax, sys_listen
 syscall
 
 mov rdi, r12
 xor rsi, rsi
 xor rdx, rdx
-mov rax, SYS_ACCEPT
+mov rax, sys_accept
 syscall
-mov r12, rax
 
-mov rdi, r12
+mov r13, rax
+mov rdi, r13
 lea rsi, [rsp+16]
 mov rdx, 4096
-mov rax, SYS_READ
+mov rax, sys_read
 syscall
 
-mov rdi, r12
+lea rsi, [rsp+16]
+xor rdi, rdi
+
+.find_method_end:
+    cmp byte [rsi+rdi], ' '
+    je .path_start
+    inc rdi
+    jmp .find_method_end
+
+.path_start:
+    inc rdi
+    mov rbx, rdi ; points at '/'
+
+.find_path_end:
+    cmp byte [rsi+rdi], ' '
+    je .path_end
+    inc rdi
+    jmp .find_path_end
+
+.path_end:
+    mov byte [rsi+rdi], 0
+
+mov r8, rax
+lea rdi, [rsi+rbx] 
+xor rsi, rsi
+mov rax, sys_open
+syscall
+
+mov r14, rax
+mov rdi, r14
+lea rsi, [rsp+r8]
+mov rdx, 256
+mov rax, sys_read
+syscall
+
+mov rdi, r14
+mov r14, rax
+mov rax, sys_close
+syscall
+
+mov rdi, r13
 lea rsi, [rel response]
 mov rdx, response_len
-mov rax, SYS_WRITE
+mov rax, sys_write
 syscall
 
-mov rdi, r12
-mov rax, SYS_CLOSE
+mov rdi, r13
+lea rsi, [rsp+r8]
+mov rdx, r14
+mov rax, sys_write
 syscall
 
-mov rdi, EXIT_SUCCESS 
-mov rax, SYS_EXIT
+mov rdi, r13
+mov rax, sys_close
+syscall
+
+mov rdi, exit_success 
+mov rax, sys_exit
 syscall
