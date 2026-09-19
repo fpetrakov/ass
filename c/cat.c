587772fe431ef214c102dbdbb873bec86dcd9b7e
fpetrakov <jakephin2015@gmail.com>
2026-09-19 10:33:16 +0300

    add -n option

--- c/cat.c
+++ c/cat.c
@@ -2,6 +2,9 @@
 #include "fcntl.h"
 #include "unistd.h"
 #include "sys/stat.h"
+#include "stdbool.h"
+#include <inttypes.h>
+#include <string.h>
 
 int main(int args, char *argv[]) {
     char buf[256];
@@ -12,6 +15,7 @@ int main(int args, char *argv[]) {
         }
         if (n == -1) perror("read");
         close(STDIN_FILENO);
+        return 0;
     }
 
     struct stat st;
@@ -32,11 +36,44 @@ int main(int args, char *argv[]) {
         }
 
         ssize_t n;
-        while ((n = read(fd, buf, sizeof buf))> 0) {
-            write(STDOUT_FILENO, buf, n);
+        size_t line = 1;
+        int at_start = true;
+
+        const char *option_n = "-n";
+        bool is_option_n = strcmp(argv[1], option_n);
+        if (is_option_n == 0) {
+            while ((n = read(fd, buf, sizeof buf))> 0) {
+                const char *p = buf, *end = buf + n;
+
+                while (p < end) {
+                    if (at_start) {
+                        char prefix[32];
+                        int len = snprintf(prefix, sizeof prefix, "%zu ", line);
+                        write(STDOUT_FILENO, prefix, len);
+                        at_start = false;
+                    }
+
+                    const char *nl = memchr(p, '\n', end - p);
+                    if (nl) {
+                        write(STDOUT_FILENO, p, nl - p + 1);
+                        p = nl + 1;
+                        line++;
+                        at_start = true;
+                    } else {
+                       write(STDOUT_FILENO, p, end - p);
+                       at_start = false;
+                    }
+                }
+            }
+        } else {
+            while ((n = read(fd, buf, sizeof buf)) > 0) {
+                write(STDOUT_FILENO, buf, n);
+            }
         }
+
         if (n == -1) perror("read");
         close(fd);
     }
+
     return 0;
 }
