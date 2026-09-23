f20ca1dc04f8f35f7f7abba877cff56bfa2fd678
fpetrakov <jakephin2015@gmail.com>
2026-09-23 12:41:24 +0300

    isogram

--- zzz/src/isogram.zig
+++ zzz/src/isogram.zig
@@ -1,4 +1,16 @@
+const std = @import("std");
+
 pub fn isIsogram(str: []const u8) bool {
-    _ = str;
-    @compileError("please implement the isIsogram function");
+    const NUM_OF_ALPHABET_LETTERS = 26;
+    var found = [_]bool{false} ** NUM_OF_ALPHABET_LETTERS;
+
+    for (0..str.len) |i| {
+        const char = std.ascii.toLower(str[i]);
+        if (char == ' ') continue;
+        if (char == '-') continue;
+        if (found[char - 'a']) return false;
+        found[char - 'a'] = true;
+    }
+
+    return true;
 }
